6b636f6ffe2754fb57730e48c13553479a62c258
Rive Sunder <rivesunder@protonmail.com>
2020-02-05 18:52:00 +0100

    first commit

---
+++ openi/__init__.py
@@ -0,0 +1,18 @@
+from gym.envs.registration import register
+
+"""
+register(\
+        id="MezzaLuna-v0",\
+        entry_point="openi.envs:MezzaLunaEnv",\
+        )
+
+register(\
+        id="Popper-v0",\
+        entry_point="openi.envs:Popper",\
+        )
+
+register(\
+        id="Tripod-v0",\
+        entry_point="openi.envs:Tripod",\
+        )
+"""
