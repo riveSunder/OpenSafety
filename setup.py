3d09adda4896234b6c47f84bd327a08d10322b05
RiveSunder <RiveSunder@protonmail.com>
2021-01-26 15:29:32 -0700

    rename folder

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
  
-setup(name="open_safety_gym",
+setup(name="open_safety",
       version='0.0.1',
-      packages=["open_safety_gym"],
+      packages=["open_safety"],
       install_requires=['gym',
                         'pybullet']
 )
