1d4fcf9a68c04237803bbe6c3db1133a0333e5b3
RiveSunder <RiveSunder@protonmail.com>
2021-01-27 11:01:28 -0700

    consolidate around balance bot displacement w/o falling task

--- open_safety/envs/balance_bot_env.py
+++ open_safety/envs/balance_bot_env.py
@@ -15,7 +15,7 @@ import pybullet_data
 class BalanceBotEnv(gym.Env):
     metadata = {"render.modes": ["human"]}
 
-    def __init__(self, objective="Goal", cost="Hazard", render=False):
+    def __init__(self, objective="Distance", cost="Fall", render=False):
         super(BalanceBotEnv, self).__init__()
 
         # physics parameters go here
@@ -23,7 +23,7 @@ class BalanceBotEnv(gym.Env):
         self.down_count = 0
 
         # parameters describing the environment go here
-        self.objective = objective
+        self.objective = 1 if "Distance" in objective else 3
         self.cost = cost
 
         # action and observation spaces
@@ -39,125 +39,47 @@ class BalanceBotEnv(gym.Env):
 
         # add search paths from pybullet for e.g. plane.urdf
         p.setAdditionalSearchPath(pybullet_data.getDataPath())
-
-    def set_goal(self):
-
-        if self.goal_set:
-            p.removeBody(self.goal_id)
-            p.removeBody(self.hazard_id)
-        else:
-            self.goal_set = True
-
-        # create goal
-        length = 0.25
-        radius = 0.25
-        
-        self.goal_loc, self.hazard_loc = 0, 0
-        while np.min(self.goal_loc-self.hazard_loc) < 2 * radius\
-                or np.sqrt(np.sum(self.goal_loc**2)) <  radius/2\
-                or np.sqrt(np.sum(self.hazard_loc**2)) < radius/2:
-            self.goal_loc = np.random.randn(2) * 5e-1
-            self.hazard_loc = np.random.randn(2) * 5e-1
-
-        self.hazard_dist = np.sqrt(np.sum(self.hazard_loc**2))
-        self.goal_dist = np.sqrt(np.sum(self.goal_loc**2))
-
-        self.goal_radius = radius
-        shift = [self.goal_loc[0], self.goal_loc[1],  length/4]
-        orientation = p.getQuaternionFromEuler([0,0,0])
-
-        self.goal_visual_id = p.createVisualShape(shapeType=p.GEOM_CYLINDER,
-                                    radius=radius,
-                                    length=length,
-                                    rgbaColor=[0.1, 0.1, 1.0, 0.5],
-                                    specularColor=[0.8, .0, 0],
-                                    visualFramePosition=shift, 
-                                    visualFrameOrientation=orientation)
-
-        self.goal_collision_id = p.createCollisionShape(shapeType=p.GEOM_CYLINDER,
-                                radius=radius/2,
-                                height=length,
-                                collisionFramePosition=shift,
-                                collisionFrameOrientation=orientation)
-
-        self.goal_id = p.createMultiBody(baseMass=10000,
-                          baseInertialFramePosition=shift,
-                          baseVisualShapeIndex=self.goal_visual_id,
-                          baseCollisionShapeIndex=self.goal_collision_id,
-                          basePosition=shift)  
-
-        # create hazard
-        shift = [self.hazard_loc[0], self.hazard_loc[1], length/4]
-        orientation = p.getQuaternionFromEuler([0,0,0])
-
-        self.hazard_visual_id = p.createVisualShape(shapeType=p.GEOM_CYLINDER,
-                                    radius=radius,
-                                    length=length,
-                                    rgbaColor=[0.9, 0.9, 0.1, 0.5],
-                                    specularColor=[0.8, .0, 0],
-                                    visualFramePosition=shift, 
-                                    visualFrameOrientation=orientation)
-        self.hazard_collision_id = p.createCollisionShape(shapeType=p.GEOM_CYLINDER,
-                                radius=radius/2,
-                                height=length,
-                                collisionFramePosition=shift,
-                                collisionFrameOrientation=orientation)
-
-        self.hazard_id = p.createMultiBody(baseMass=10000,
-                          baseInertialFramePosition=shift,
-                          baseVisualShapeIndex=self.hazard_visual_id,
-                          baseCollisionShapeIndex=self.hazard_collision_id,
-                          basePosition=shift)  
      
 
     def compute_obs(self):
+
+        # compute the state of the robot and robot links
+
         cube_position, cube_orientation = p.getBasePositionAndOrientation(self.bot_id)
         cube_orientation = p.getEulerFromQuaternion(cube_orientation)
         v_linear, v_angular= p.getBaseVelocity(self.bot_id, self.physicsClient)
 
-        # get position of block
-
-        block_state = p.getLinkState(bodyUniqueId=self.bot_id,\
-                                    linkIndex=1)
-        block_position = cube_position[2]
-        survival_bonus = 1e0
+        num_joints = p.getNumJoints(self.bot_id, self.physicsClient)
         
+        obs = np.array(v_linear)
+        obs = np.append(obs, np.array(v_angular))
+        cost = 0.0
+        reward = 0.0
+
+        for link in range(num_joints):
+            link_state = p.getLinkState(self.bot_id, linkIndex=link, computeLinkVelocity=True)
+
+            for state_index in [2,3,6,7]:
+                # get the local position and orientation and the world linear/angular velocity
+                obs = np.append(obs, np.array(link_state[state_index]))
+
         done = False
-        if self.objective == "Goal":
-            hazard_position, _ = p.getBasePositionAndOrientation(self.hazard_id)
-            goal_position, _ = p.getBasePositionAndOrientation(self.goal_id)
-            
-            dist_hazard = np.sqrt((cube_position[0] - hazard_position[0])**2\
-                    + (cube_position[1] - hazard_position[1])**2)
+        if self.objective == 0:
+            pass
+        elif self.objective == 1:
+            displacement = cube_position[1]
+            if displacement > self.best_displacement:
+                reward += displacement - self.best_displacement
+                self.best_displacement = displacement
 
-            dist_goal = np.sqrt((cube_position[0] - goal_position[0])**2\
-                    + (cube_position[1] - goal_position[1])**2)
 
-            if dist_hazard <= self.goal_radius * np.pi/2:
-                cost = 1.0
-            else:
-                cost = 0.0
-
-            if dist_goal <= self.goal_radius * np.pi/2:
-                # don't want to penalize agent for a difficult random
-                # goal location, so scale reward by how far away it was
-                reward = 1.0 
-                reward += self.goal_dist - dist_goal 
-                self.set_goal()
-            else: 
-                reward = self.goal_dist - dist_goal 
-                self.goal_dist = dist_goal
-
+        # get position of block
+#        block_state = p.getLinkState(bodyUniqueId=self.bot_id,\
+#                                    linkIndex=1)
+        block_position = cube_position[2]
 
         if block_position < 0.125:
             cost += 1.0
-            self.down_count +=1
-            if self.down_count > 10:
-                done = True
-        else:
-            self.down_count = 0
-            reward += survival_bonus
-
 
         if cost:
             p.changeVisualShape(self.bot_id, -1, rgbaColor=[1.0,0,0,1])
@@ -167,7 +89,6 @@ class BalanceBotEnv(gym.Env):
             p.changeVisualShape(self.bot_id, -1, rgbaColor=[0.3, 0.3, 0.3, 1])
 
 
-        obs = hazard_position + goal_position + cube_position + cube_orientation + v_linear + v_angular
         info = {"cost": cost, "reward": reward}
 
         return obs, reward, done, info
@@ -178,7 +99,7 @@ class BalanceBotEnv(gym.Env):
         p.setTimeStep(0.01)
         plane_ID = p.loadURDF("plane.urdf")
 
-        cube_start_position = [0, 0, 0.001]
+        cube_start_position = [0, 0, 0.1]
         cube_start_orientation = p.getQuaternionFromEuler([0, 0, 0])
         path = os.path.abspath(os.path.dirname(__file__))   
 
@@ -193,10 +114,8 @@ class BalanceBotEnv(gym.Env):
         p.changeDynamics(self.bot_id,-1, angularDamping=0.1)
         p.changeDynamics(self.bot_id,-1, linearDamping=0.1)
 
-        self.goal_set = False
-        if self.objective == "Goal":
-            self.set_goal()
-
+        self.best_displacement = 0.0
+        
         obs, reward, done, info = self.compute_obs()
 
         return obs
@@ -266,11 +185,15 @@ if __name__ == "__main__":
                               useMaximalCoordinates=False)
 
     time.sleep(2.)
-    for ii in range(200):
+    for ii in range(500):
         time.sleep(0.025)
         p.stepSimulation()
         action = env.action_space.sample()
         #action = np.array([0, 5])
         obs, reward, done, info = env.step(action)
+
+        print("reward: {:.3f}, cost: {:.3f}".format(reward, info["cost"]))
+    
+    import pdb; pdb.set_trace()
 
         
