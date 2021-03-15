744d0153bd13c30b326e3799eb1b068be25fdc40
RiveSunder <RiveSunder@protonmail.com>
2021-03-14 22:38:59 -0600

    register envs with gym

--- open_safety/envs/__init__.py
+++ open_safety/envs/__init__.py
@@ -1,3 +1,17 @@
-#from openi.envs.mezza_luna_env import MezzaLunaEnv
-#from openi.envs.popper_env import PopperEnv
-#from openi.envs.tripod_luna_env import TripodEnv
+from gym.envs.registration import register
+
+register(id="BalanceBotEnv-v0", \
+        entry_point="open_safety.envs.balance_bot_env:BalanceBotEnv")
+register(id="DuckBalanceBotEnv-v0", \
+        entry_point="open_safety.envs.balance_bot_env:DuckBalanceBotEnv")
+register(id="CubeBalanceBotEnv-v0", \
+        entry_point="open_safety.envs.balance_bot_env:CubeBalanceBotEnv")
+register(id="SphereBalanceBotEnv-v0", \
+        entry_point="open_safety.envs.balance_bot_env:SphereBalanceBotEnv")
+
+register(id="DuckMinitaurEnv-v0", \
+        entry_point="open_safety.envs.minitaur_env:MinitaurDuckBulletEnv")
+register(id="CubeMinitaurEnv-v0", \
+        entry_point="open_safety.envs.minitaur_env:MinitaurCubeBulletEnv")
+register(id="SphereMinitaurEnv-v0", \
+        entry_point="open_safety.envs.minitaur_env:MinitaurSphereBulletEnv")
